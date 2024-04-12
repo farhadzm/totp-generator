e58fcfebaea390657b005a241dbc78582438ccf1
Farhad Zamani <f.zamani@deltaban.local>
2024-04-12 15:36:23 +0330

    register services in SampleApi

--- TotpGenerator.SampleApi/Program.cs
+++ TotpGenerator.SampleApi/Program.cs
@@ -1,8 +1,14 @@
+using TotpGenerator.SampleApi.Contracts;
+using TotpGenerator.SampleApi.Options;
+using TotpGenerator.SampleApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.Configure<UserOption>(builder.Configuration.GetSection("UserOption"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
